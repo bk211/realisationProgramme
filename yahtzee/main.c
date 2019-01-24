47e289d9b082dc3f28246cdd9539e195f65eef09
CAI Chaolei <bk211@piscine-12.bocal.cs.univ-paris8.fr>
2019-01-24 12:03:31 +0100

    creation brouillon déroulement jeu

--- yahtzee/main.c
+++ yahtzee/main.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+#include <mecanismeJeu.h>
+#include <comparaison.h>
+int main(int argc, char const *argv[])
+{
+
+	int nombreJoueur;
+	int nombreTour;
+	//printf("Entrez le nombre de joueur\n");
+	//scanf("%d",&nombreJoueur); 
+// en option, pour le moment le developpement se limite a 2 joueur
+	printf("Entrez le nombre de tour\n");
+	scanf("%d",&nombreTour); 
+
+	while (nombreTour > 0){
+
+		int i;//ici 2
+		for (numeroJoueur = 0; i < nombreJoueur; ++i){
+			lancerUnTour(numeroJoueur);
+		}
+
+		comparerScore(joueur1,joueur2);
+		//compare le score des 2, donne le gagnant du tour
+		//>> incremente score gagant
+
+	}
+
+//	fin des tours 
+	
+	compareScoreTotale(joueur1,joueur2);
+	//>> felicite le gagant en affichant le score des 2
+	return 0;
+}
