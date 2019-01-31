fe5de6daa3ea1bcb46fd10faf595ef0b55e7282d
Playmaker <chaoleicai@gmail.com>
2019-01-31 12:31:12 +0100

    fin boucle main,affichage a ajouter

--- yahtzee/main.c
+++ yahtzee/main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <mecanismeJeu.h>
-#include <comparaison.h>
+
+/*
+#include <comparaison.h>*/
+
+
 int main(int argc, char const *argv[])
 {
+	Player player1;
+	Player player2;
 
-	int nombreJoueur;
-	int nombreTour;
+	int nombreTour=1;
+
+	//int nombreJoueur;
 	//printf("Entrez le nombre de joueur\n");
 	//scanf("%d",&nombreJoueur); 
 // en option, pour le moment le developpement se limite a 2 joueur
-	printf("Entrez le nombre de tour\n");
-	scanf("%d",&nombreTour); 
+//	printf("Entrez le nombre de tour\n");
+//	scanf("%d",&nombreTour); 
 
 	while (nombreTour > 0){
 
-		int i;//ici 2
-		for (numeroJoueur = 0; i < nombreJoueur; ++i){
-			lancerUnTour(numeroJoueur);
-		}
-
+		completeTurn(player1);
+		completeTurn(player2);
 		comparerScore(joueur1,joueur2);
 		//compare le score des 2, donne le gagnant du tour
 		//>> incremente score gagant
@@ -29,5 +33,7 @@ int main(int argc, char const *argv[])
 	
 	compareScoreTotale(joueur1,joueur2);
 	//>> felicite le gagant en affichant le score des 2
+
+
 	return 0;
 }
