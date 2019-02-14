c5ddc0c54355eb7273cc492e1a94fdcf0b61b0aa
DANABAL Ramu <rdanabal@piscine-10.bocal.cs.univ-paris8.fr>
2019-02-14 11:37:42 +0100

    Commenté oxy

--- navalfight/create_grid.c
+++ navalfight/create_grid.c
@@ -1,4 +1,13 @@
 #include "navalfight.h"
+
+/**
+ * \brief      Vérifie si on reste bien dans la grille.
+ * \details    Vérifie que le nombre de colonne et ligne est bien inférieurs aux limites.
+ * \param    indice_col     Le nombre de colonne.
+ * \param    indice_line    Le nombre de ligne .
+ * \return   Renvoie un entier 0 si tout ce passe bien, sinon 1.
+ */
+
 int ft_test_born(int indice_col,int indice_line){
 	if (indice_col > 16)
 		return 1;
@@ -10,11 +19,33 @@ int ft_test_born(int indice_col,int indice_line){
 		return 1;
 	return 0;
 }
+
+/**
+ * \brief     Test s'il y a un bateau
+ * \details   Verifie s'il y a bateau aux coordonées passé
+ * \param     **grid      La grille de la bataille navale.
+ * \param     indice_col    Première coordonée.
+ * \param     indice_line   Seconde coordonée.
+ * \return    Renvoie un entier 1 s'il y a un bateau, sinon 0.
+ */
+
+
 int ft_test_boat(char **grid, int indice_col, int indice_line){
 	if (grid[indice_line][indice_col] == 'O')
 		return 1;
 	return 0;
 }
+
+/**
+ * \brief     Place un bateau de dimension 2.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
+
 int ft_boat_1_2(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -55,6 +86,18 @@ int ft_boat_1_2(char **grid, int indice_col, int indice_line){
 	}
 	return 1;
 }
+
+
+/**
+ * \brief     Place un bateau de dimension 3.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
+
 int ft_boat_1_3(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -102,6 +145,18 @@ int ft_boat_1_3(char **grid, int indice_col, int indice_line){
 	}
 	return 1;
 }
+
+
+/**
+ * \brief     Place un bateau de dimension 4.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
+
 int ft_boat_1_4(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -168,6 +223,18 @@ int ft_boat_1_4(char **grid, int indice_col, int indice_line){
 	}
 	return 1;
 }
+
+
+/**
+ * \brief     Place un bateau de dimension 5.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
+
 int ft_boat_1_5(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -247,6 +314,18 @@ int ft_boat_1_5(char **grid, int indice_col, int indice_line){
 	}
 	return 1;
 }
+
+
+/**
+ * \brief     Place un bateau de dimension 6.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
+
 int ft_boat_1_6(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -339,6 +418,17 @@ if (ft_test_born(conteur_col - 1, conteur_line) == 0){
 	}
 	return 1;
 }
+
+/**
+ * \brief     Place un bateau de dimension 4.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
+
 int ft_boat_2_2(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -397,6 +487,17 @@ int ft_boat_2_2(char **grid, int indice_col, int indice_line){
 	}
 	return 1;
 }
+
+/**
+ * \brief     Place un bateau de dimension 8.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
+
 int ft_boat_2_4(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -495,6 +596,16 @@ int ft_boat_2_4(char **grid, int indice_col, int indice_line){
 	}
 	return 1;
 }
+
+/**
+ * \brief     Place un bateau de dimension 6.
+ * \details   Vérifie s'il y a des bateaux aux alentours des coordonées passées, si non
+ *             un bateau est placé en verifiant que l'on ne sors pas de la grille.      
+ * \param    **grid         Grille de la bataille navale .
+ * \param    indice_col     Premiere coordonée.
+ * \param    indice_line    Seconde coordonée.
+ * \return   Renvoie un entier qui vaut 0 si le bateau a été placé, sinon 0.
+ */
 int ft_boat_1_4_1_3(char **grid, int indice_col, int indice_line){
 	int conteur_col = indice_col;
 	int conteur_line = indice_line;
@@ -583,6 +694,16 @@ int ft_boat_1_4_1_3(char **grid, int indice_col, int indice_line){
 	}
 	return 1;
 }
+
+/**
+ * \brief     Fonction qui remplie la grille.
+ * \details   Choisis des coordonnées aléatoires et appel les fonctions qui place bateaux avec
+              ces coordonées .      
+ * \param    **grid         Grille de la bataille navale .
+ * \return   Renvoie un char ** qui correspond à la grille remplie . 
+ */
+
+
 char **ft_fill_grid(char **grid){
 	int indice_col = (rand() % 17);
 	int indice_line = (rand() % 17);
@@ -634,6 +755,14 @@ char **ft_fill_grid(char **grid){
 	}
 	return grid;
 }
+
+
+/**
+ * \brief     Fonction qui crée la grille.
+ * \details   Crée la grille, puis applique ft_fill_grid sur la grille pour la remplir .      
+ * \return    Renvoie un char **  qui correspond à la grille.
+ */
+
 char **cree_tableau(void){
 	char **grid;
 	int conteur_col;
