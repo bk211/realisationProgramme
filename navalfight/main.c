2dca73012a3ca2a87f689e171df67764f9c9d9c0
DANABAL Ramu <rdanabal@piscine-10.bocal.cs.univ-paris8.fr>
2019-02-14 10:58:04 +0100

    Main commenté

--- navalfight/main.c
+++ navalfight/main.c
@@ -1,4 +1,11 @@
 #include "navalfight.h"
+/**
+ * \brief      Le main du programme
+ * \details    Créer les grilles et appel la boucle princpale
+ *              
+ * \return   Renvoie 0, si tout ce passe bien.
+ */
+
 int main() {
 	srand(time(NULL));
 	char ** grid1 = cree_tableau();
