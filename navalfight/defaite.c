4d15aa4ceeaf3f605417a6f5b94a076dd7471745
DANABAL Ramu <rdanabal@piscine-10.bocal.cs.univ-paris8.fr>
2019-02-14 10:54:15 +0100

    Commentaire fini

--- navalfight/defaite.c
+++ navalfight/defaite.c
@@ -1,4 +1,12 @@
 #include "navalfight.h"
+/**
+ * \brief       Test s'il reste au moin un bateau non coulé
+ * \details    Parcours la grille et vérifie s'il reste au moin un 'O' , c'est a dire au moin un bateau non coulé, sinon tout les bateaux sont coulés
+ *                
+ * \param    **grid         La grille est passé en paramètre .
+ * \return    Renvoie un entier 1 si il reste au moin un bateau, 0 si tout les bateaux sont coulés.
+ */
+
 int ft_detect_defaite(char **grid){
   int conteur_col;
   int conteur_line;
