b8a2a3bd679295b51d6f3edd61ca6ebcde924324
BK211 <chaoleicai@gmail.com>
2019-02-14 13:19:47 +0100

    extend color choices

--- yahtzee/color.c
+++ yahtzee/color.c
@@ -12,14 +12,49 @@ void red () {
   printf("\033[1;31m");
 }
 
-void yellow() {
-  printf("\033[1;33m");
-}
 void green() {
   printf("\033[1;32m");
 }
+
+void yellow() {
+  printf("\033[1;33m");
+}
+
+void blue() {
+  printf("\033[1;34m");
+}
+
+void magenta() {
+  printf("\033[1;35m");
+}
+
+void cyan() {
+  printf("\033[1;36m");
+}
+
+
+
 
 void resetColor () {
   printf("\033[0m");
 }
 
+
+int main(int argc, char const *argv[])
+{
+    red();
+    printf("hello\n");
+    green();
+    printf("hello\n");
+    yellow();
+    printf("hello\n");
+    blue();
+    printf("hello\n");
+    magenta();
+    printf("hello\n");
+    cyan();
+    printf("hello\n");
+    resetColor();
+    printf("hello\n");
+    return 0;
+}
