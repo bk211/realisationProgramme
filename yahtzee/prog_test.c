1aee5af9dece9fe031f44b368ccf4babb34c82ed
Playmaker <chaoleicai@gmail.com>
2019-03-14 10:35:04 +0100

    add test saisit double

--- yahtzee/prog_test.c
+++ yahtzee/prog_test.c
@@ -2,6 +2,32 @@
 #include<stdio.h>
 #include<assert.h>
 #include"score.c"
+
+
+
+int TESTsaisitScore(int *tab,int *tabFinal){
+  int answer=0;
+  while(1){
+    printf("Saisissez le numero de la case que vous voulez remplir  \n");
+    //boucle_de_saisie(&answer,1,15); // ligne originale
+    answer = 1;//modifier ici 
+    /*
+    le test saisit double est valide car s'il la case est deja remplit, on revient au debut de la boucle
+      while, la saisit est redemande a utilisateur
+    */
+    
+    --answer;
+    if(tabFinal[answer] >= 0){
+      printf("La case est deja remplit, merci de choisir une autre\n");
+    }
+    else{
+      tabFinal[answer] = tab[answer];
+      return 1;
+    }
+
+  }
+}
+
 int main() {
   int test_yaht[8]={1,1,1,1,1,1,1};
   int test_PS[8]={7,3,2,6,5,4,1};
@@ -19,6 +45,10 @@ int main() {
   int test_fullv1_false[8]={8,8,7,3,8,3,8};
   int test_fullv2_false[8]={3,2,3,5,2,3,2};
 
+  int tab_partie[]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int tab_partie_final[]= {-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+  assert(TESTsaisitScore(tab_partie, tab_partie_final));
   // les assert pour cas OK
   assert(testYahtzee(test_yaht) == 50);
 
